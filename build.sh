07f7237309b84b49aac4b6029837b6a48234aa96
Farid Delgado <farid.delgado@thomsonreuters.com>
2022-10-07 03:47:49 -0400

    - minor change

--- build.sh
+++ build.sh
@@ -30,11 +30,6 @@ else
   echo "codebuild git hash: $hash"
 fi
 
-export CommitHash=$hash
-echo "CommitHash: $CommitHash"
-CommitHash=$hash
-echo "CommitHash: $CommitHash"
-
 # ECR login for the current account in order to publish our image
 aws ecr get-login-password --region ${region} \
 | docker login --username AWS --password-stdin ${account}.dkr.ecr.${region}.amazonaws.com
